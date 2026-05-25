1cd774ffaa9662b8660814cba52afc81d8d4c765
Virgilio Padilla Ruiz <vpadillar8@gmail.com>
2026-05-25 00:30:52 -0500

    Corrección paso 2 del formulario de registro de solicitud

--- web/modules/custom/asocolderma_inscription/src/Form/SolicitudIngresoWizardForm.php
+++ web/modules/custom/asocolderma_inscription/src/Form/SolicitudIngresoWizardForm.php
@@ -798,11 +798,13 @@ final class SolicitudIngresoWizardForm extends FormBase
       'field_departamento' => ['target_id' => (int) $wizard_values['general']['departamento'],],
       'field_ciudad_ejercicio' => ['target_id' => (int) $wizard_values['general']['ciudad_ejercicio'],],
 
-      'field_direccion_institucional' => $wizard_values['contacto']['direccion'] ?? '',
-      'field_correspondencia_fisica' => $wizard_values['contacto']['correspondencia_fisica'] ?? '',
-      'field_telefono' => $wizard_values['contacto']['telefono'],
+      'field_correspondencia_fisica' => $wizard_values['contacto']['direccion'] ?? '',
+      'field_direccion_institucional' => $wizard_values['contacto']['correspondencia_fisica'] ?? '',
+      'field_email_principal' => $wizard_values['contacto']['email'] ?? '',
       'field_celular' => $wizard_values['contacto']['celular_full'] ?? $wizard_values['contacto']['celular'] ?? '',
-      'field_email_principal' => $wizard_values['contacto']['email'],
+      'field_lugar_correspondencia' => !empty($wizard_values['contacto']['lugar_correspondencia'])
+        ? ['target_id' => (int) $wizard_values['contacto']['lugar_correspondencia']]
+        : NULL,
 
       'field_universidad' => $wizard_values['profesional']['universidad'],
       'field_titulo' => $wizard_values['profesional']['titulo'],
